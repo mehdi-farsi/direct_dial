16692d312048e7ce12c312bcbf8b62c752a17e6d
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2014-11-15 14:16:58 +0100

    format XML for answerURL response

--- app.js
+++ app.js
@@ -2,31 +2,28 @@ var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 var plivo = require('plivo-node');
-var response = plivo.Response();
 
 var server = http.createServer(function(req, res) {
+    var response = plivo.Response();
     var page = url.parse(req.url).pathname;
     var params = querystring.parse(url.parse(req.url).query);
 
     res.writeHead(200, {"Content-Type": "text/xml"});
     if (page == '/dial') {
-      var toNumber = 'To' in params ? params['To'] : '';
-      var fromNumber = 'CLID' in params ? params['CLID'] : 'From' ? params['From'] : '';
-      var callerName = 'CallerName' in params ? params['CallerName'] : '';
+      var toNumber = params['To'] || '';
+      var fromNumber = params['CLID'] || (params['From'] || '');
+      var callerName = params['CallerName'] || '';
 
       if (toNumber == "") {
-        console.log('Hangup');
         response.addHangup();
-      } else if (toNumber.substring(0,3) == "sip:") {
-        console.log('SIP');
-
+      } else if (toNumber.substring(0, 4) == "sip:") {
+        var dial = response.addDial({'callerName' : callerName});
+        dial.addUser(toNumber);
       } else {
-
+        var dial = response.addDial({'callerId' : fromNumber});
+        dial.addNumber(toNumber);
       };
-      console.log("To: " + toNumber);
-      console.log("From: " + fromNumber);
-      console.log("Caller Name: " + callerName);
     }
-    res.end();
+    res.end(response.toXML());
 });
 server.listen(process.env.PORT || 8080);
